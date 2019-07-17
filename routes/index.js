23464333957fc0ca80c89b907feeb92510c23048
Ben Remnant <ben.remnant@gmail.com>
2019-07-18 09:35:41 +1200

    Updated front page and added about us page.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
+/* HOME */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+
+/* ABOUT US */
+
+router.get('/aboutus', function(req, res, next) {
+  res.render('aboutus');
+});
+router.get('/ourpeople', function(req, res, next) {
+  res.render('ourpeople');
+});
+router.get('/ourhistory', function(req, res, next) {
+  res.render('ourhistory');
+});
+
 
 module.exports = router;
